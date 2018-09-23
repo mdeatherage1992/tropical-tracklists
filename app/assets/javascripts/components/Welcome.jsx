b48758da743ccb93e6e1f8ecb00567085ef7675c
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-09-23 15:13:32 -0400

    working react component

--- app/assets/javascripts/components/Welcome.jsx
+++
@@ -1,5 +0,0 @@
-var Welcome = React.createClass({
-  render: function () {
-   return <h1>Welcome!</h1>
-  }
-})
---
+++ app/assets/javascripts/components/server.js
@@ -0,0 +1,7 @@
+var Welcome = createReactClass({
+  render: function () {
+   return (
+     <h1>Welcome!</h1>
+   )
+  }
+})
